92e758bbadf57ade719c051377696045900ebf1c
joaomatamosquito <grosso.joao@escola.pr.gov.br>
2025-07-01 10:50:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,19 @@
-criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    container.appendChild(cartao)
 
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-
-criaCartao(
-    'Programação',
-    'Em que ano foi criada a ONU? ?',
-    '1945'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual país tem mais títulos da Copa do Mundo? ?',
-    'Brasil'
-)
-
-criaCartao(
-    'Programação',
-    'Qual programa da Netflix teve mais visualizações em 2021?',
-    'Squid Game'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Em que país está localizada a usina nuclear de Chernobyl?',
-    'Ucrânia'
-)
-
+}
