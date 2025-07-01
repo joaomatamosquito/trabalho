d0779e496690f23fef26cd7b6b91d49b207ceb82
joaomatamosquito <grosso.joao@escola.pr.gov.br>
2025-07-01 08:19:58 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Programação',
+    'Em que ano foi criada a ONU? ?',
+    '1945'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país tem mais títulos da Copa do Mundo? ?',
+    'Brasil'
+)
+
+criaCartao(
+    'Programação',
+    'Qual programa da Netflix teve mais visualizações em 2021?',
+    'Squid Game'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Em que país está localizada a usina nuclear de Chernobyl?',
+    'Ucrânia'
+)
+
